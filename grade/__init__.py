c39a351d4ed1ad175d62524542f46c7a8ea12e5c
Harley <harley.ufpr@gmail.com>
2021-04-11 05:23:42 -0300

    mudanças

--- grade/__init__.py
+++ grade/__init__.py
@@ -6,6 +6,4 @@ import pandas as pd
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0fb59abbe2bb39814d80f1d1'
 
-
-
 from grade import routes
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from grade import app
 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
